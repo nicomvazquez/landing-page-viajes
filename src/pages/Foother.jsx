f3a869c01aaf2e58860d618fc24cbe04b676196a
nicomvazquez <vazquezpizzinicolas@gmail.com>
2024-01-28 01:18:02 -0300

    correcion foother

--- src/pages/Foother.jsx
+++ src/pages/Foother.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Foother() {
   return (
-    <div name="contact" className="flex justify-center items-center py-10 bg-neutral-800">
+    <div name="contact" className="flex justify-center items-center py-10 bg-neutral-900">
       <div className="w-3/5 flex flex-col justify-between gap-10">
-        <p className="text-2xl text-neutral-400">
+        <p className="text-2xl text-amber-700">
           ¿Listo para vivir la aventura de tu vida? Contáctanos hoy para
           planificar tu próxima experiencia con ExploraNatura.
         </p>
